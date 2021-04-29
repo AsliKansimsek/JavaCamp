2a628c67ce8ef466496c49732610804b524dd2c0
AsliKansimsek <83128405+AsliKansimsek@users.noreply.github.com>
2021-04-29 16:56:37 +0300

    Ders#3

--- oopIntro/src/oopIntro/Main.java
+++ oopIntro/src/oopIntro/Main.java
@@ -4,20 +4,32 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Product product1 = new Product(1, "Lenovo V14", 15000, "16 GB Ram"); //referans oluşturma, instance
+		Product product1 = new Product(1, "Lenovo V14", 15000, "16 GB Ram",10); //referans oluşturma, instance
 
 		
 		Product product2 = new Product(); 
-		product2.id = 2;
-		product2.name = "Lenovo V15";
-		product2.unitPrice = 16000;
-		product2.detail = "32 GB Ram";
+		product2.setId(2);
+		product2.setName("Lenovo V15");
+		product2.setDetail("16GB Ram");
+		product2.setDiscount(10);
+		product2.setUnitPrice(16000);
 		
+		System.out.println(product2.getUnitPriceAfterDiscount());
+		
+		Category category1 = new Category();
+		category1.setId(1);
+		category1.setName("İçecek");
+		
+		Category category2 = new Category();
+		category2.setId(2);
+		category2.setName("Yiyecek");
+		
+		System.out.println(category1.getName());
+		System.out.println(category2.getName());
+		
+		/*
 		Product product3 = new Product(); 
-		product3.id = 3;
-		product3.name = "Hp 5";
-		product3.unitPrice = 10000;
-		product3.detail = "8 GB Ram";
+
 		
 		//CLASS LAR REFERANS TİPTİR.
 		Product[] products = {product1,product2,product3};
@@ -44,5 +56,6 @@ public class Main {
 		productManager.addToCart(product2);
 
 		productManager.addToCart(product3);
+		*/
 	}
 }
